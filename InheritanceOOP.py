f6db5e8e841786b7a59f57377ef141cb3476fd23
titomatu <tamatu@gmail.com>
2021-08-03 20:55:57 -0500

    Inheritance VII

--- InheritanceOOP.py
+++ InheritanceOOP.py
@@ -13,7 +13,34 @@ class Vehiculo:
         print("marca: ", self.__marca, " - modelo: ", self.__modelo)
 
 class Moto(Vehiculo):
+    __hcaballito = ""
+
+    def hcaballito(self):
+        self.__hcaballito = "Voy a hacer el caballito"
+    
+    #Sobreescritura de métodos
+    def estado(self):
+        print("Vas a hacer el caballito?? ", self.__hcaballito)
+
+class VehiculoE:
+    __cargando = False
+
+    def __init__(self):
+        self.__autonomia = 100
+    
+    def cargar(self):
+        self.__cargando = True
+
+#Herencia Múltiple
+#Se le da preferencia a la primera clase que está en el paréntesis
+class BicicletaE(Vehiculo, VehiculoE):
     pass
 
 mimoto = Moto("Honda", "CBR")
+mimoto.hcaballito()
 mimoto.estado()
+
+#Herencia Múltiple
+bicie = BicicletaE("Tern", "D16")
+bicie.cargar()
+bicie.estado()
